c208606436436b25a2a78c1e16e4d8a9efb411ce
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-04-12 22:42:13 +0300

    Add the card number state

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+function reducer(model, action){
+  switch(action.type) {
+    case 'SET-CARD-NUMBER':
+      model.cardNumber = action.payload.split("");
+      return model;
+    default:
+      return model;
+  }
+}
+
+var store = Redux.createStore(reducer, {
+  cardNumber: []
+});
+
+const e = React.createElement;
+
 function doubleEverySecondDigitFromRight(digits) {
   return digits.reverse().map(function (digit, index) {
     return index % 2 === 0 ? digit : digit * 2;
@@ -24,45 +40,46 @@ function luhn(cardNumber) {
 
 function render() {
   ReactDOM.render (
-    React.createElement('div', {className: 'credit-card'}, [
-      React.createElement('div', {className: 'credit-card-inner'}, [
-        React.createElement('div', {className: 'credit-card-front'}, [
-          React.createElement('div', {className: 'chip'}, [
-            React.createElement('div', {className: 'side-top-left'}, ''),
-            React.createElement('div', {className: 'top'}, ''),
-            React.createElement('div', {className: 'center'}, ''),
-            React.createElement('div', {className: 'bottom'}, ''),
-            React.createElement('div', {className: 'side-right'}, ''),
-            React.createElement('div', {className: 'vertical-top'}, ''),
-            React.createElement('div', {className: 'vertical-center'}, ''),
-            React.createElement('div', {className: 'vertical-bottom'}, ''),
+    React.createElement(ReactRedux.Provider, { store: store }, [
+      React.createElement('div', {className: 'credit-card'}, [
+        React.createElement('div', {className: 'credit-card-inner'}, [
+          React.createElement('div', {className: 'credit-card-front'}, [
+            React.createElement('div', {className: 'chip'}, [
+              React.createElement('div', {className: 'side-top-left'}, ''),
+              React.createElement('div', {className: 'top'}, ''),
+              React.createElement('div', {className: 'center'}, ''),
+              React.createElement('div', {className: 'bottom'}, ''),
+              React.createElement('div', {className: 'side-right'}, ''),
+              React.createElement('div', {className: 'vertical-top'}, ''),
+              React.createElement('div', {className: 'vertical-center'}, ''),
+              React.createElement('div', {className: 'vertical-bottom'}, ''),
+            ]),
+            React.createElement('div', {className: 'credit-card-number'}, [
+              React.createElement('input', {type: 'text', maxLength: '16', onInput: function(event) {
+                store.dispatch({type: 'SET-CARD-NUMBER', payload: event.target.value})
+                }}, null),
+              React.createElement('span', {className: 'metal first-col'}, store.getState().cardNumber.slice(0, 4)),
+              React.createElement('span', {className: 'metal second-col'}, store.getState().cardNumber.slice(4, 8)),
+              React.createElement('span', {className: 'metal third-col'}, store.getState().cardNumber.slice(8, 12)),
+              React.createElement('span', {className: 'metal fourth-col'}, store.getState().cardNumber.slice(12, 16)),
+              React.createElement('span', {className: 'metal second-row first-col'}, store.getState().cardNumber.slice(0, 4)),
+              React.createElement('span', {className: 'metal second-row second-col'}, store.getState().cardNumber.slice(4, 8)),
+              React.createElement('span', {className: 'metal second-row third-col'}, store.getState().cardNumber.slice(8, 12)),
+              React.createElement('span', {className: 'metal second-row fourth-col'}, store.getState().cardNumber.slice(12, 16))
+            ]),
+            React.createElement('div', {className: 'expiry-dates'}, [
+              React.createElement('input', {type: 'text'}, null),
+              React.createElement('span', {className: 'dates'}, 'MM/YY'),
+              React.createElement('span', {className: 'dates second-row'}, 'MM/YY')
+            ]),
+            React.createElement('div', {className: 'card-holder'}, [
+              React.createElement('input', {type: 'text'}, null),
+              React.createElement('span', {className: 'metal'}, 'Alexander Petrov'),
+              React.createElement('span', {className: 'metal second-row'}, 'Alexander Petrov')
+            ])
           ]),
-          React.createElement('div', {className: 'credit-card-number'}, [
-            React.createElement('input', {type: 'text'}, null),
-            React.createElement('span', {className: 'metal first-col'}, '0000'),
-            React.createElement('span', {className: 'metal second-col'}, '0000'),
-            React.createElement('span', {className: 'metal third-col'}, '0000'),
-            React.createElement('span', {className: 'metal fourth-col'}, '0000'),
-            React.createElement('span', {className: 'metal second-row first-col'}, '0000'),
-            React.createElement('span', {className: 'metal second-row second-col'}, '0000'),
-            React.createElement('span', {className: 'metal second-row third-col'}, '0000'),
-            React.createElement('span', {className: 'metal second-row fourth-col'}, '0000')
-          ]),
-          React.createElement('div', {className: 'expiry-dates'}, [
-            React.createElement('input', {type: 'text'}, null),
-            React.createElement('span', {className: 'dates'}, 'MM/YY'),
-            React.createElement('span', {className: 'dates second-row'}, 'MM/YY')
-          ]),
-          React.createElement('div', {className: 'card-holder'}, [
-            React.createElement('input', {type: 'text'}, null),
-            React.createElement('span', {className: 'metal'}, 'Alexander Petrov'),
-            React.createElement('span', {className: 'metal second-row'}, 'Alexander Petrov')
-          ])
-        ]),
-        React.createElement('div', {className: 'credit-card-back'}, [
-
         ])
-      ])
+      ]),
     ]),
     document.getElementById('root')
   )
@@ -75,4 +92,5 @@ function render() {
   document.querySelector('.credit-card').appendChild(pattern.toCanvas());
 }
 
+store.subscribe(function() { render(); });
 render();
