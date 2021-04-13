4dcf67564ba223013e7b947dd5bb5d0f2171a082
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-04-13 15:17:39 +0300

    Add placeholder value for the card number input

--- main.js
+++ main.js
@@ -51,50 +51,52 @@ function render() {
   var expiryDate = state.expiryDate.slice(0, 2).join('') + '/'
   + store.getState().expiryDate.slice(2, 4).join('');
 
+  var firstCol = state.cardNumber.length ? state.cardNumber.slice(0, 4) : '0000';
+  var secondCol = state.cardNumber.length ? state.cardNumber.slice(4, 8) : '0000';
+  var thirdCol = state.cardNumber.length ? state.cardNumber.slice(8, 12) : '0000';
+  var fourthCol = state.cardNumber.length ? state.cardNumber.slice(12, 16) : '0000';
+
   ReactDOM.render (
     e(ReactRedux.Provider, { store: store }, [
       e('div', {className: 'credit-card'}, [
-        e('div', {className: 'credit-card-inner'}, [
-          e('div', {className: 'credit-card-front'}, [
-            e('div', {className: 'chip'}, [
-              e('div', {className: 'side-top-left'}, ''),
-              e('div', {className: 'top'}, ''),
-              e('div', {className: 'center'}, ''),
-              e('div', {className: 'bottom'}, ''),
-              e('div', {className: 'side-right'}, ''),
-              e('div', {className: 'vertical-top'}, ''),
-              e('div', {className: 'vertical-center'}, ''),
-              e('div', {className: 'vertical-bottom'}, ''),
-            ]),
-            e('div', {className: 'credit-card-number'}, [
-              e('input', {type: 'text', maxLength: '16', onInput: function(event) {
-                store.dispatch({type: 'SET-CARD-NUMBER', payload: event.target.value})
-                }}, null),
-              e('span', {className: 'metal first-col'}, state.cardNumber.slice(0, 4)),
-              e('span', {className: 'metal second-col'}, state.cardNumber.slice(4, 8)),
-              e('span', {className: 'metal third-col'}, state.cardNumber.slice(8, 12)),
-              e('span', {className: 'metal fourth-col'}, state.cardNumber.slice(12, 16)),
-              e('span', {className: 'metal second-row first-col'}, state.cardNumber.slice(0, 4)),
-              e('span', {className: 'metal second-row second-col'}, state.cardNumber.slice(4, 8)),
-              e('span', {className: 'metal second-row third-col'}, state.cardNumber.slice(8, 12)),
-              e('span', {className: 'metal second-row fourth-col'}, state.cardNumber.slice(12, 16))
-            ]),
-            e('div', {className: 'expiry-dates'}, [
-              e('input', {type: 'text', maxLength: '4', onInput: function(event) {
-                store.dispatch({type: 'SET-EXPIRY-DATE', payload: event.target.value})
-              }}, null),
-              e('span', {className: 'dates'}, expiryDate),
-              e('span', {className: 'dates second-row'}, expiryDate)
-            ]),
-            e('div', {className: 'card-holder'}, [
-              e('input', {type: 'text', onInput: function(event) {
-                store.dispatch({type: 'SET-CARD-HOLDER', payload: event.target.value})
-              }}, null),
-              e('span', {className: 'metal'}, state.cardHolder),
-              e('span', {className: 'metal second-row'}, state.cardHolder)
-            ])
-          ]),
-        ])
+        e('div', {className: 'chip'}, [
+          e('div', {className: 'side-top-left'}, ''),
+          e('div', {className: 'top'}, ''),
+          e('div', {className: 'center'}, ''),
+          e('div', {className: 'bottom'}, ''),
+          e('div', {className: 'side-right'}, ''),
+          e('div', {className: 'vertical-top'}, ''),
+          e('div', {className: 'vertical-center'}, ''),
+          e('div', {className: 'vertical-bottom'}, ''),
+        ]),
+        e('div', {className: 'credit-card-number'}, [
+          e('input', {type: 'text', maxLength: '16', onInput: function(event) {
+            store.dispatch({type: 'SET-CARD-NUMBER', payload: event.target.value})
+            }}, null),
+          e('span', {className: 'metal first-col'}, firstCol),
+          e('span', {className: 'metal second-col'}, secondCol),
+          e('span', {className: 'metal third-col'}, thirdCol),
+          e('span', {className: 'metal fourth-col'}, fourthCol),
+          e('span', {className: 'metal second-row first-col'}, firstCol),
+          e('span', {className: 'metal second-row second-col'}, secondCol),
+          e('span', {className: 'metal second-row third-col'}, thirdCol),
+          e('span', {className: 'metal second-row fourth-col'}, fourthCol)
+        ]),
+        e('div', {className: 'expiry-dates'}, [
+          e('input', {type: 'text', maxLength: '4', onInput: function(event) {
+            store.dispatch({type: 'SET-EXPIRY-DATE', payload: event.target.value})
+          }}, null),
+          e('span', {className: 'dates'}, expiryDate),
+          e('span', {className: 'dates second-row'}, expiryDate)
+        ]),
+        e('div', {className: 'card-holder'}, [
+          e('input', {type: 'text', onInput: function(event) {
+            store.dispatch({type: 'SET-CARD-HOLDER', payload: event.target.value})
+          }}, null),
+          e('span', {className: 'metal'}, state.cardHolder),
+          e('span', {className: 'metal second-row'}, state.cardHolder)
+        ]),
+        e('img', {className: 'logo', src: './img/master_card.jpg'}, null)
       ]),
     ]),
     document.getElementById('root')
